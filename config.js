7da410f7c4bdf900477c3c83d12a37d14ca69b03
hnt1306ks <108657043+hnt1306ks@users.noreply.github.com>
2022-07-04 14:48:14 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'bạn Thư xinh đẹp';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'hộp quà bí mật tặng cho';
+const titleCard = 'Happy Birthday bạn Thư xinh đẹp';
+const contentCard = 'Chúc bạn Thư ngày càng học giỏi, mau ăn chóng lớn và ngày càng xinh đẹp nhé :3';
+const giftImage = 'mthu2.png';
+//const base64 = '';
+//const giftImageBase64 = "data:image/png;base64, " + base64;
