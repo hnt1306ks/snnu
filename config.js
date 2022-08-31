ff10474e51387887c9cbe9904426a3a9119a2bec
hnt1306ks <108657043+hnt1306ks@users.noreply.github.com>
2022-09-01 06:49:19 +0700

    Update config.js
    Signed-off-by: hnt1306ks <108657043+hnt1306ks@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'bạn Thư xinh đẹp';
+const nameGirl = 'to: Nu';
 const giftUrl = 'https://www.facebook.com/minhthu.haha';
-const eventName = 'hộp quà bí mật tặng cho';
-const titleCard = 'Happy Birthday bạn Thư xinh đẹp';
-const contentCard = 'Chúc bạn Thư ngày càng học giỏi, mau ăn chóng lớn và ngày càng xinh đẹp nhé :3';
+const eventName = 'From: Hưng';
+const titleCard = 'Happy Birthday';
+const contentCard = 'Chúc bạn Nu ngày càng học giỏi, mau ăn chóng lớn và ngày càng xinh đẹp nhé :))';
 const giftImage = 'mthu2.png';
 //const base64 = '';
 //const giftImageBase64 = "data:image/png;base64, " + base64;
